8416cc4937025fbf85591d3b48529173a1fced98
Simston <stephane.simon51@gmail.com>
2023-06-06 22:05:05 +0200

    Added air01.js

---
+++ air01.js
@@ -0,0 +1,40 @@
+const MyTools = require('./MyTools');
+const myTools = new MyTools();
+const ARGS_ARRAY = process.argv;
+const STRING_SEPARATOR = ARGS_ARRAY[3];
+
+const splitStringWithStringSeparator = (stringBeforeCut, separator) => {
+    const FINAL_ARRAY = [];
+    let matchCount = 0;
+    let string = "";
+   
+    for(let i = 0; i < stringBeforeCut.length; i++){   
+        string += stringBeforeCut[i];
+
+        if(stringBeforeCut[i] === separator[matchCount] ){    
+            matchCount += 1;
+        }else{
+            matchCount = 0;
+        }        
+        if(matchCount === separator.length){
+            string = string.slice(0, string.length - separator.length);
+            FINAL_ARRAY.push(string);
+            string = "";
+            matchCount = 0;
+        }    
+
+        if(i == stringBeforeCut.length -1){
+            FINAL_ARRAY.push(string);
+        }
+    }
+    //console.log(string)
+    return FINAL_ARRAY.join("\n");
+}
+
+// Error Handling
+const ARGS_OK = myTools.checkArgumentCount(2, ARGS_ARRAY);
+if(!ARGS_OK)  console.log("Veuillez entrer une chaîne de caractères ainsi que le sépérateur souhaité.");
+// Display
+if(ARGS_OK){
+    console.log(splitStringWithStringSeparator(ARGS_ARRAY[2], STRING_SEPARATOR));
+}
