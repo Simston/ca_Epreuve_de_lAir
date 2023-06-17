e765d40762516ed3bcb4cf70f76cfac23f34790a
Simston <stephane.simon51@gmail.com>
2023-06-17 16:48:10 +0200

    Added air08.js myRotation

---
+++ air08.js
@@ -0,0 +1,30 @@
+const MyTools = require('./MyTools');
+const myTools = new MyTools();
+const commandLineArgs = process.argv;
+
+const myRotation = inputArray => {
+    let tmp = "";
+    tmp = inputArray.shift(); // Remove and stock first element
+    inputArray.push(tmp); // Push the first element at the end of array
+    return inputArray;
+}
+
+const getArgumentsIntoArray = args => {
+    let inputArray = [];
+
+    for( let i = 2; i < args.length ; i++){
+        inputArray.push(args[i]);
+    }
+    return inputArray;
+}
+
+// Error Handling
+const hasMinArgsCount = myTools.checkMinArgumentCount(3, commandLineArgs);
+if (!hasMinArgsCount) {
+    console.log("Veuillez entrer au moins 3 éléments.");
+} else {
+    // parsing
+    const inputArray = getArgumentsIntoArray(commandLineArgs)
+    // display
+    console.log(myRotation(inputArray));
+}
